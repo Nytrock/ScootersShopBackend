7e9278d3d0e863d906c8c2f607ab697810ac026e
Nytrock <savvazhirnov@gmail.com>
2024-11-16 15:54:37 +0300

    Vercel another css fix

--- app_main/settings.py
+++ app_main/settings.py
@@ -91,7 +91,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- app_main/urls.py
+++ app_main/urls.py
@@ -12,5 +12,4 @@ urlpatterns = [
     path('users/', include('users.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
