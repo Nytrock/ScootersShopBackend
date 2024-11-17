dcf8de69749d2af6037d509db6e96d4c6b651fc7
Nytrock <savvazhirnov@gmail.com>
2024-11-17 08:35:02 +0300

    Fix admin panel

--- scooters/admin.py
+++ scooters/admin.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
-from django.template.defaultfilters import truncatechars
 
 from scooters.models import Scooter
 
 
 @admin.register(Scooter)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'price', 'short_description')
-
-    def short_description(self, obj):
-        return truncatechars(obj.description, 35)
-    short_description.short_description = 'Описание'
+    list_display = ('id', 'title', 'max_speed', 'weight', 'price')
